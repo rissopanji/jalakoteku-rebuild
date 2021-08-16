f30c27dad744d07c29edf310b2ac90291a1fca47
Ichsan Nulmuhlis <ichsan.nulmuhlis@gmail.com>
2021-08-16 19:14:31 +0700

    navbar

--- public/script/main.js
+++ public/script/main.js
@@ -4,3 +4,18 @@ const menu = document.getElementById('menu');
 btn_menu.addEventListener('click', () =>{
   menu.classList.toggle('hidden');
 })
+
+// ADD CLASS ON NAVBAR WHEN SCROLL
+$(window).on('scroll', function () {
+  if ($(window).scrollTop()) {
+    $('nav').removeClass('lg:shadow-none lg:py-3 lg:text-base');
+    $('nav').addClass('lg:shadow-md lg:bg-gray1 lg:py-1 lg:text-sm');
+    $('#logobrand').removeClass('lg:h-14');
+    $('#logobrand').addClass('lg:h-10');
+  } else {
+    $('nav').removeClass('lg:shadow-md lg:py-1 lg:bg-gray1 lg:text-sm');
+    $('nav').addClass('lg:shadow-none lg:py-3 lg:text-base');
+    $('#logobrand').removeClass('lg:h-10');
+    $('#logobrand').addClass('lg:h-14');
+  }
+})
